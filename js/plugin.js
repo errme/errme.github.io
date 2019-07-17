e2a8846a97ea2995ae768070a5c8b33c553b0871
苍耳 <1459829778@qq.com>
2019-07-17 15:21:35 +0800

    me

--- js/plugin.js
+++ js/plugin.js
@@ -1,5 +1,5 @@
 /*
- Inspired by the lightbox plugin adapted to jquery by Leandro Vieira Pinho (http://leandrovieira.com)
+
  @author  : Nicolas Turlais : nicolas-at-insipi.de
  @version : V0.3.1 - June 2012
  @license : Licensed under CCAttribution-ShareAlike
