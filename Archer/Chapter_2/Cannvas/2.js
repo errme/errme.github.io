db94938a5238c0ef56aad6e71e90b171e4bcf3f6
Esefairy <1459829778@qq.com>
2021-01-18 18:10:38 +0800

    ni

--- Archer/Chapter_2/Cannvas/2.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.getElementById("canvasElem");
-var context = canvas.getContext('2d');
-canvas.width = 300;
-canvas.height = 200;
-canvas.style.border = '2px solid blue';
-context.strokeRect(100, 50, 70, 100);
-context.fillRect(100, 50, 70, 100);
