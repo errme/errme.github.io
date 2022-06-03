6e42d9d851a4c3edee14d7d82a469c9922fea6e8
me <1459829778@qq.com>
2022-06-03 08:57:02 +0800

    me

--- nian/include/product/Diaspora-0/404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div id="p404">
-    <h2>Not Found</h2>
-    <p>
-        The page you were looking for is no longer available.
-        <a href="/"> - Back Home -</a>
-    </p>
-</div>
